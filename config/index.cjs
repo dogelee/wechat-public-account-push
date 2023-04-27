7b892e082cf9c21e7ce3b0ac4e2d2bad09131b49
dogelee <130121910+dogelee@users.noreply.github.com>
2023-04-27 10:13:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,15 +13,15 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '30d61b84149159dd67873cf9810b9829',
 
+  PROVINCE: '邵阳',
+  CITY: '邵阳县',
+  
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obRq86enMQQaEkPYXlDhElKKPGF4',
-      
-      PROVINCE: '邵阳',
-      CITY: '邵阳县',
   
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'j9fEswjMJMT2aryafnGmJH1IHNJJLwRHqcxLKydZ8-Q',
