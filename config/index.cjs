ac70b08f8e438026e5a8a414fe14de042d643ee4
dogelee <130121910+dogelee@users.noreply.github.com>
2023-04-27 09:25:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,9 +47,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'May_',
+      name: '去见你',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obRq86aOJRTvY5oXtjNkRtLbjBUU',
+      id: 'obRq86enMQQaEkPYXlDhElKKPGF4',
     }
   ],
 
