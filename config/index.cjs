081661040b3fde0f9bd35a7a9972595a0d84eec0
dogelee <130121910+dogelee@users.noreply.github.com>
2023-04-27 09:52:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obRq86aOJRTvY5oXtjNkRtLbjBUU',
+      id: 'obRq86enMQQaEkPYXlDhElKKPGF4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'j9fEswjMJMT2aryafnGmJH1IHNJJLwRHqcxLKydZ8-Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
